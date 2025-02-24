8ba45f22e6f3f63106e811ad9d4d194bb859eebf
Karthik-0756 <karthikmani0756@gmail.com>
2025-02-24 11:38:43 +0530

    cors

--- app.js
+++ app.js
@@ -16,7 +16,19 @@ const PassengerCount = require("./model/PassengerCountSchema");
 
 var app = express();
 const server = http.createServer(app);
-app.use(cors({ origin: "http://localhost:3001" }));
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 // ✅ Stable MongoDB Connection
 const uri = 'mongodb+srv://karthik:Karthik%400756@cluster0.zxheq.mongodb.net/Atlas';
